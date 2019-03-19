dc88094d9055b6cc27681afd0bac62fb8fa48bf8
jimfuego <32339090+jimfuego@users.noreply.github.com>
2019-03-19 16:48:17 -0700

    Create new_user.js

---
+++ imports/api/new_user.js
@@ -0,0 +1,41 @@
+import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
+
+
+export const New_User = new Mongo.Collection("new_user");
+
+// if (Meteor.isServer) {
+//
+//
+//   Meteor.publish("new_user", function guessesPublish() {
+//     return New_User
+//       .find({}, {
+//         // FIXME: don't limit or sort... maybe sort
+//         limit: 10,
+//         sort: {
+//           createdAt: -1
+//         }
+//       });
+//   });
+// }
+
+Meteor.methods({
+  "users.insert"(user)  {
+    check(user, String);
+
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    New_User.insert({
+      userName : user.userName,
+      password: user.password,
+      gamesPlayed: 0,
+      gamesWon: 0,
+      createdAt : Date.now(),
+      player : Meteor.user().username
+    });
+  }
+});
