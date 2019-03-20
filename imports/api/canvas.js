baa1aa6123fe8f6adac2e58bc4bd444b97bd24d4
jimmyb <jimmyb89@yahoo.com>
2019-03-20 14:56:31 -0700

    add canvas.js

---
+++ imports/api/canvas.js
@@ -0,0 +1,36 @@
+import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
+
+
+export const Canvas = new Mongo.Collection("canvas");
+
+if (Meteor.isServer) {
+  Meteor.publish("canvas", function playersPublish() {
+    return Canvas.find({});
+  });
+}
+
+//save state of canvas
+Meteor.methods({
+  "canvas.saveCanvas"(canvasSave)  {
+    check(canvasSave, String);
+
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Canvas.remove({});
+    Canvas.insert({
+      canvasState : canvasSave
+    });
+  }
+});
+
+//delete canvas
+Meteor.methods({
+  "canvas.deleteCanvas"()  {
+    Canvas.remove({});
+  }
+});
