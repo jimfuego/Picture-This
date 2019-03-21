5a1289bd25a1c6fa900d56dba13c77b4b78adb2c
Abasiama Akpan <akpan.a@husky.neu.edu>
2019-03-20 21:27:57 -0700

    Attempt 2

--- imports/ui/Creategame.jsx
+++ imports/ui/Creategame.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {Answer} from "../api/answer.js";
-import {
- BrowserRouter as Router,
- Route
-} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import {Meteor} from "meteor/meteor";
 //import PropTypes from "prop-types";
 //import { Meteor } from "meteor/meteor";
@@ -31,47 +28,54 @@ class Creategame extends React.Component {
     alert("A name was submitted: " + this.state.value);
     this.setState({value: document.getElementById('picName').value});
     event.preventDefault();
-    if(Meteor.call("answer.checkInProgress", (err, res) => {
-  	if(err){
-  		alert("alert Creategame.jsx.handleSubmit");
-  		console.log(err);
-    		return;
-    	}
-    })){
-    	alert("Game already in progress, rerouting to game lobby");
-    	this.props.history.push("/otherusers");
-    }
-    else{
-    	Meteor.call("answer.insert",this.state.value, (err,res) => {
-    	if (err){
-    		alert("Error recording answer");
-    		console.log(err);
-    		return;
-    	}
-    	console.log("Answer inserted", res);
-    	this.props.history.push("/drawer");
-    	this.setState({
-    		value: ""
-    	});
-    		
-    });
-	}
+    Meteor.call("answer.checkInProgress", (err, res) => {
+      if(err){
+        alert("alert Creategame.jsx.handleSubmit");
+        console.log(err);
+        return;
+      } else {
+        if(res == true){
+                                        // Game is in progress
+                                        alert("Game already in progress, rerouting to game lobby");
+                                        this.props.history.push("/otherusers");
+                                      } else {
+                                        Meteor.call("answer.insert",this.state.value, (err,res) => {
+                                          if (err){
+                                            alert("Error recording answer");
+                                            console.log(err);
+                                            return;
+                                          }
+                                          console.log("Answer inserted", res);
+                                          this.props.history.push("/drawer");
+                                          this.setState({
+                                            value: ""
+                                          });
+
+                                        });
+                                      }
+
+
+                                    }
+                                  });
+
+
   }
+
 
   render() {
     return (
-    <div className="formClass">
-       <h2 className="startgame" text-align="center">Start Game</h2>
+      <div className="formClass">
+      <h2 className="startgame" text-align="center">Start Game</h2>
       <form className="gameform" onSubmit={this.handleSubmit}>
-        <label className="picturelabel">
-          Enter Picture Name:
-          <input type="text" id="picName" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit"/>
+      <label className="picturelabel">
+      Enter Picture Name:
+      <input type="text" id="picName" value={this.state.value} onChange={this.handleChange} />
+      </label>
+      <input type="submit" value="Submit"/>
       </form>
       </div>
-    );
+      );
   }
 }
 
-export default Creategame;
+export default withRouter(Creategame);
